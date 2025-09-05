caacb73086c2c1722ed6caeb6a66d0e821d803e3
masaton <masamasamasato0216@gmail.com>
2025-09-05 11:51:04 +0900

    Update version.php

--- config/version.php
+++ config/version.php
@@ -8,9 +8,11 @@ return [
     |--------------------------------------------------------------------------
     |
     | This is the version of Connect-CMS. If you want to hide version information
+    | from manage page, set 'show_cc_version' to false.
     |
     */
+
     'cc_version' => '2.40.1',
 
-
+    'show_cc_version' => true,
 ];
